1a6e3118e46e79d649d04d4c4a487c8dbfb37b43
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-04-04 01:17:11 +0500

    H/W(task2)

--- tasks/tasks/Program.cs
+++ tasks/tasks/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            Console.Write($"m: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write($"n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write($"k: ");
+            int k = int.Parse(Console.ReadLine());
+            int[][] arr = new int[m][];
+            int l = k-1;
+            if(l>=0 && l<m)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = new int[n];
+                    for (int j = 0; j < arr[i].Length; j++)
+                    {
+                        arr[i][j] = random.Next(1, 10);
+                        Console.Write($" {arr[i][j]}");
+                    }
+                    Console.WriteLine();
+                }
+                int sum = 0;
+                int multiply = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    for (int i = 0; i < m; i++)
+                    {
+                        if(l==j)
+                        {
+                            sum += arr[i][j];
+                            multiply *= arr[i][j];
+                        }
+                    }
+                }
+                Console.WriteLine($"sum in column k: {sum}");
+                Console.WriteLine($"multiply in column k: {multiply}");
+            }
         }
     }
 }
